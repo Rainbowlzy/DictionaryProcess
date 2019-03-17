ee426518ead4e4d5fbc65360815cecd22f1af7a0
Rainbow Lu <rainbowluzy@hotmail.com>
2019-03-18 07:37:18 +0800

    init

---
+++ DictionaryProcess/Funcs.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DictionaryProcess
+{
+    public class Funcs
+    {
+        private static readonly Dictionary<string, string> PascalCaseDictionary = new Dictionary<string, string>();
+        private static readonly Dictionary<string, string> TranslateDictionary = new Dictionary<string, string>();
+
+        private static readonly Dictionary<string, string> CheckDuplicateCHS = new Dictionary<string, string>();
+        private static readonly Dictionary<string, string> CheckDuplicateCommon = new Dictionary<string, string>();
+
+        public static string AddComments(string xml) => $"<!-- {xml} -->";
+
+        public static string MakeChsItem(string name, string comment = "")
+        {
+            var pascalCase = PascalCase(name);
+            var key = pascalCase.ToLower();
+            if (CheckDuplicateCHS.ContainsKey(key))
+            {
+                return AddComments(string.Join(Environment.NewLine,
+                    $"duplicate in chs : {pascalCase} with {name} origin {CheckDuplicateCHS[key]}",
+                    (MakeItem(pascalCase, name, comment))));
+            }
+
+            CheckDuplicateCHS.Add(key, name);
+            return MakeItem(pascalCase, name, comment);
+        }
+
+        public static string MakeCommonItem(string name, string comment = "")
+        {
+            var pascalCase = PascalCase(name);
+            var key = pascalCase.ToLower();
+            if (CheckDuplicateCommon.ContainsKey(key))
+            {
+                return AddComments(string.Join(Environment.NewLine,
+                    $"duplicate in common : {pascalCase} with {name} origin {CheckDuplicateCommon[key]}",
+                    MakeItem(pascalCase, Translate(name), comment)));
+            }
+
+            CheckDuplicateCommon.Add(key, name);
+            return MakeItem(pascalCase, Translate(name), comment);
+        }
+
+        public static string MakeItem(string key, string val, string comment = "") =>
+            $"<data name=\"{key}\" xml:space=\"preserve\">"
+            + $"<value>{val}</value>"
+            + $"<comment>{comment}</comment>"
+            + "</data>";
+
+        public static string PascalCase(string key)
+        {
+            if (PascalCaseDictionary.ContainsKey(key)) return PascalCaseDictionary[key];
+            key = key?.Trim("@,?.|\\/".ToCharArray());
+            var pascalCase = HttpGet(string.Format("http://localhost/Translator/api/PascalCase?key={0}", key))
+                .Trim('"', '.', '-', '+', ',', '\'', '?')
+                .Replace("'", string.Empty);
+            PascalCaseDictionary.Add(key, pascalCase);
+            return pascalCase;
+        }
+
+        public static string Translate(string key)
+        {
+            if (TranslateDictionary.ContainsKey(key)) return PascalCaseDictionary[key];
+            key = key?.Trim("@,?.|\\/".ToCharArray());
+            var pascalCase = HttpGet(string.Format("http://localhost/Translator/api/Translate?key={0}", key))
+                .Trim('"', '.', '-', '+', ',', '\'', '?')
+                .Replace("'", string.Empty);
+            TranslateDictionary.Add(key, pascalCase);
+            return pascalCase;
+        }
+
+        private static HttpWebResponse CreateGetHttpResponse(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("url");
+            }
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "GET";
+            return request.GetResponse() as HttpWebResponse;
+        }
+
+        private static string HttpGet(string url)
+        {
+            try
+            {
+                Stream responseStream = CreateGetHttpResponse(url).GetResponseStream();
+                string buf = string.Empty;
+                if (responseStream != null)
+                {
+                    using (StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("UTF-8")))
+                    {
+                        buf = reader.ReadToEnd();
+                    }
+
+                    responseStream.Close();
+                }
+
+                return buf;
+            }
+            catch (Exception e)
+            {
+                File.WriteAllText(
+                    $"D:\\log\\Generator_Err_{DateTime.Now.ToString("yyyy MMMM dd")}_{DateTime.Now.Ticks}.log",
+                    JsonConvert.SerializeObject(new
+                    {
+                        project = "Generator",
+                        method = nameof(HttpGet),
+                        url,
+                        e
+                    }));
+                return string.Empty;
+            }
+        }
+
+        public static void Show(IEnumerable<string> chs)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, chs));
+        }
+    }
+}
---
+++ DictionaryProcess/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using static DictionaryProcess.Funcs;
+
+namespace DictionaryProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var common_resx_path =
+                @"E:\source\repos\GongYi.La\src\AgileBoost.Gongyila.Languages\MobilePortal.resx";
+            var chs_resx_path =
+                @"E:\source\repos\GongYi.La\src\AgileBoost.Gongyila.Languages\MobilePortal.zh-CN.resx";
+            var text = File.ReadAllText(@"D:\big_dictionary.txt");
+            var chs = new HashSet<string>(Regex.Matches(text, "\\\"[\u4e00-\u9fa5]+([!！。.,，：《》【】()（）{}、/\\|@$￥0-9\\-a-zA-Z]*[\u4e00-\u9fa5]*)+\\\"")
+                .Select(m => m.Value))
+                .OrderByDescending(k => k.Length);
+            Console.WriteLine(string.Concat(Enumerable.Repeat("-", 100)));
+
+            Show(chs.Take(1050));
+            Console.ReadLine();
+
+
+            File.WriteAllText(common_resx_path,
+                File.ReadAllText(common_resx_path).Replace("</root>",
+                    string.Join(Environment.NewLine, chs.Select(c => MakeCommonItem(c, c)))
+                    + "</root>"));
+            File.WriteAllText(chs_resx_path,
+                File.ReadAllText(chs_resx_path).Replace("</root>",
+                    string.Join(Environment.NewLine, chs.Select(c => MakeChsItem(c, c)))
+                    + "</root>"));
+
+            Console.WriteLine(string.Concat(Enumerable.Repeat("-", 100)));
+            var folder = @"E:\source\repos\GongYi.La\src\AgileBoost.Gongyila.Mobile\";
+            var cshtmls = Directory.GetFiles(folder, "*.cshtml", SearchOption.AllDirectories);
+            foreach (string cshtml in cshtmls)
+            {
+                File.WriteAllLines(cshtml, File.ReadAllLines(cshtml).Select(ParseCshtmlLine));
+            }
+        }
+
+        private static string ParseCshtmlLine(string line)
+        {
+            var target = line;
+//            var matches = Regex.Matches(line, "[\u4e00-\u9fa5]+").Select(t => t.Value);
+            var matches = Regex.Matches(line, "[\u4e00-\u9fa5]+").Select(t => t.Value);
+            var enumerable = matches as string[] ?? matches.ToArray();
+            foreach (string match in enumerable)
+            {
+                var matchIndex = line.IndexOf(match, StringComparison.Ordinal);
+                var doubleQuotationIndex = line.IndexOf("\"", StringComparison.Ordinal);
+                var doubleSlashIndex = line.IndexOf("//", StringComparison.Ordinal);
+                var blockSlashIndex = line.IndexOf("/*", StringComparison.Ordinal);
+                var blockAtIndex = line.IndexOf("@*", StringComparison.Ordinal);
+                if (matchIndex != -1 && (
+                        false
+//                        || doubleQuotationIndex != -1 && doubleQuotationIndex < matchIndex
+                        || doubleSlashIndex != -1 && doubleSlashIndex < matchIndex
+                        || blockSlashIndex != -1 && blockSlashIndex < matchIndex
+                        || blockAtIndex != -1 && blockAtIndex < matchIndex
+                    ))
+                {
+                    continue;
+                }
+                target = target.Replace(match, $"@(AgileBoost.Gongyila.Mobile.App_GlobalResources.MobilePortal.{PascalCase(match)})");
+            }
+            return target;
+        }
+    }
+}
